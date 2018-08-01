d45f20e1e7add9108f0b764426baf832dcb2b2bd
Illia Levandovskyi <lis2889@gmail.com>
2018-08-01 17:03:44 +0300

    event store antipattern

---
+++ ShoppingCart/Model/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ShoppingCart.Model
+{
+    public struct Event
+    {
+        public int SequenceNumber { get; }
+        public DateTime OccurredAt { get; }
+        public string Name { get; }
+        public object Content { get; }
+
+        public Event(int sequenceNumber, DateTime occurredAt, string name, object content)
+        {
+            SequenceNumber = sequenceNumber;
+            OccurredAt = occurredAt;
+            Name = name;
+            Content = content;
+        }
+    }
+}
--- ShoppingCart/Services/IEventStore.cs
+++ ShoppingCart/Services/IEventStore.cs
@@ -1,40 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using ShoppingCart.Model;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
 
 namespace ShoppingCart.Services
 {
     public interface IEventStore
     {
-        void Raise(string eventName, object content);
-        IEnumerable GetEvents(int first, int last);
-    }
-
-    public class EventStore : IEventStore
-    {
-        private static readonly Dictionary<int, EventData> _storage = new Dictionary<int, EventData>();
-        public void Raise(string eventName, object content)
-        {
-            var id = _storage.Count + 1;
-            _storage.Add(id, new EventData()
-            {
-                EventName = eventName,
-                Content = content,
-                Offset = DateTime.UtcNow
-            });
-        }
-
-        public IEnumerable GetEvents(int first, int last)
-        {
-            return _storage.Skip(first - 1).Take(last - first).Select(p => p.Value);
-        }
-
-        private class EventData
-        {
-            public string EventName { get; set; }
-            public object Content { get; set; }
-            public DateTime Offset { get; set; }
-        }
+        Task Raise(string eventName, object content);
+        Task<IEnumerable<Event>> GetEvents(int first, int last);
     }
 }
